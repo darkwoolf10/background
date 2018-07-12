2daa3294e512dbce83168a552229497051b89159
darkwoolf10 <woolftreuser@yandex.ru>
2018-07-12 13:08:19 +0300

    and crontab and launching every minute

--- background.py
+++ background.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import os
-import datetime
+import os, os.path
+import random
 
-time = int(str(datetime.datetime.now().time()).split(":")[0])
+DIR = '/home/woolf/Workspace/background/images'
 
-if time < 12:
-    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/woolf/Pictures/AbeKislevitz_BambooForest_CD27.jpg")
-elif time > 12 and time < 20:
-    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/woolf/Pictures/Tidal-Wave-Wallpapers-Gallery-71-Plus-PIC-WPT302629.jpg")
+count_wallpaper = (len([name for name in os.listdir(DIR)]))
+
+if os.listdir(DIR):
+    random_image = random.randint(1,count_wallpaper)
+    print("Change image")
+    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri " + DIR + "/" + str(random_image) + ".jpg")
 else:
-    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/woolf/Pictures/paint-stains-surface.jpg")
+    print("Not found files in " + str(DIR))
